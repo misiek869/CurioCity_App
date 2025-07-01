a6654634917003c476a88cbcf8c08ea0c892d085
Michal Gawik <gawlik.zator@o2.pl>
2025-07-01 05:17:53 +0200

    create dashboard pages and layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+const DashboardLayout = ({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) => {
+	return (
+		<div className='flex justify-center items-center min-h-screen w-full'>
+			{children}
+		</div>
+	)
+}
+
+export default DashboardLayout
---
+++ src/app/(dashboard)/tours/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ToursPage = () => {
+	return <div>tours</div>
+}
+
+export default ToursPage
