cf71921d458e2db0f9d707c1a8b2c4239f5f02b2
Michal Gawik <gawlik.zator@o2.pl>
2025-07-02 05:46:41 +0200

    create links and style

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import Link from 'next/link'
+
+type Link = {
+	href: string
+	label: string
+}
+
+const links: Link[] = [
+	{ href: '/chat', label: 'chat' },
+	{ href: '/tours', label: 'tours' },
+	{ href: '/tours/new-tour', label: 'new-tour' },
+	{ href: '/profile', label: 'profile' },
+]
 
 const NavLinks = () => {
-	return <div></div>
+	return (
+		<ul className='menu text-slate-50'>
+			{links.map((link, index) => {
+				return (
+					<li key={index}>
+						<Link href={link.href} className='capitalize'>
+							{link.label}
+						</Link>
+					</li>
+				)
+			})}
+		</ul>
+	)
 }
 
 export default NavLinks
