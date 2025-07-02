2a77a9ebf26c1d5712151d493aa608b474f295d1
Michal Gawik <gawlik.zator@o2.pl>
2025-07-02 05:41:17 +0200

    style component

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { MdLocationCity } from 'react-icons/md'
+import ThemeToggle from './ThemeToggle'
 
 const SidebarHeader = () => {
-	return <div>header</div>
+	return (
+		<div className='flex items-center mb-4 gap-4 px-4'>
+			<MdLocationCity className='w-12 h-12 text-primary' />
+			<h2 className='text-xl font-bold text-primary'>CurioCity</h2>
+			<ThemeToggle />
+		</div>
+	)
 }
 
 export default SidebarHeader
