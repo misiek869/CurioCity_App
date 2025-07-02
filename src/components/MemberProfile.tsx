f3c0ecc386601a229f0ffb6f91d34d8281ae9f0e
Michal Gawik <gawlik.zator@o2.pl>
2025-07-02 05:29:42 +0200

    create component

---
+++ src/components/MemberProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MemberProfile = () => {
+	return <div></div>
+}
+
+export default MemberProfile
---
+++ src/components/SidebarHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SidebarHeader = () => {
+	return <div>header</div>
+}
+
+export default SidebarHeader
