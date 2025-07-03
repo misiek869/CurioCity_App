74d07dae42d2f026a42f86cdd6cb066d616b36b3
Michal Gawik <gawlik.zator@o2.pl>
2025-07-03 18:33:07 +0200

    set up react-hot-toast library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import Providers from './providers'
 
 import './globals.css'
 
@@ -30,7 +31,7 @@ export default function RootLayout({
 			<html lang='en'>
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-					{children}
+					<Providers>{children}</Providers>
 				</body>
 			</html>
 		</ClerkProvider>
