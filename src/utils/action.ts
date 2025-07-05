fb6d63c8f5c3ce15a4ee09233549b991ce89b637
Michal Gawik <gawlik.zator@o2.pl>
2025-07-05 05:19:58 +0200

    configure openai

--- src/utils/action.ts
+++ src/utils/action.ts
@@ -1,8 +1,27 @@
 'use server'
 
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+})
+
 export const generateChatResponse = async (
 	chatMsg: string
 ): Promise<string> => {
-	console.log(chatMsg)
+	const response = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content: 'you are a helpful assistant',
+			},
+			{ role: 'user', content: chatMsg },
+		],
+		model: 'gpt-4.1-mini',
+		temperature: 0,
+	})
+
+	console.log(response.choices[0].message)
+	console.log(response)
 	return 'awesome'
 }
