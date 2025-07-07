24b7b2557367d35f568c6914d2e018d8897b2937
Michal Gawik <gawlik.zator@o2.pl>
2025-07-07 10:12:45 +0200

    set up page structure

--- src/components/NewTour.tsx
+++ src/components/NewTour.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+'use client'
+
+const handleSubmit = (e: React.FormEvent) => {
+	e.preventDefault()
+}
 
 const NewTour = () => {
-	return <div>New Tour</div>
+	return (
+		<>
+			<form onSubmit={handleSubmit} className='max-w-2xl'>
+				<h2 className='mb-4'>Select destination</h2>
+				<div className='join w-full'>
+					<input
+						type='text'
+						className='join-item input input-bordered w-full'
+						placeholder='city'
+						name={'city'}
+						required
+					/>
+					<input
+						type='text'
+						className='join-item input input-bordered w-full'
+						placeholder='country'
+						name={'country'}
+						required
+					/>
+					<button type='submit' className='btn btn-primary join-item'>
+						Generate Tour
+					</button>
+				</div>
+			</form>
+			<div className='mt-16'></div>
+		</>
+	)
 }
 
 export default NewTour
