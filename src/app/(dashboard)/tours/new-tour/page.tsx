699452529bc34820a580f6402404db9e37c3aa5e
Michal Gawik <gawlik.zator@o2.pl>
2025-07-07 10:04:31 +0200

    render NewTour component

--- src/app/(dashboard)/tours/new-tour/page.tsx
+++ src/app/(dashboard)/tours/new-tour/page.tsx
@@ -3,10 +3,15 @@ import {
 	HydrationBoundary,
 	QueryClient,
 } from '@tanstack/react-query'
+import NewTour from '@/components/NewTour'
 
 const NewTourPage = () => {
 	const queryClient = new QueryClient()
-	return <HydrationBoundary state={dehydrate(queryClient)}></HydrationBoundary>
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<NewTour />
+		</HydrationBoundary>
+	)
 }
 
 export default NewTourPage
