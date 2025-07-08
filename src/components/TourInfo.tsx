feb49740266166292d12031314668bdc5feefcdf
Michal Gawik <gawlik.zator@o2.pl>
2025-07-08 06:22:18 +0200

    create component body to display tour info

--- src/components/TourInfo.tsx
+++ src/components/TourInfo.tsx
@@ -1,6 +1,28 @@
-const TourInfo = ({ tour }) => {
-	console.log(tour)
-	return <div>info</div>
+type TourProps = {
+	city: string
+	country: string
+	title: string
+	description: string
+	stops: string[]
+}
+
+const TourInfo = ({ tour }: TourProps) => {
+	const { title, description, stops } = tour
+	return (
+		<div className='max-w-2xl'>
+			<h1 className='text-4xl font-semibold mb-4'>{title}</h1>
+			<p className='leading-loose mb-6'>{description}</p>
+			<ul>
+				{stops.map((item, index) => {
+					return (
+						<li key={index} className='mb-4 bg-base-100 p-4'>
+							{item}
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
 }
 
 export default TourInfo
