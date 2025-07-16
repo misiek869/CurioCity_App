5a751134ff5815f553f63c4d9d7310c24289fa34
Michal Gawik <gawlik.zator@o2.pl>
2025-07-16 05:37:28 +0200

    create component

---
+++ src/components/ToursList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ToursList = () => {
+	return <div>tours list</div>
+}
+
+export default ToursList
---
+++ src/components/ToursPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ToursPage = () => {
+	return <div>tours page</div>
+}
+
+export default ToursPage
