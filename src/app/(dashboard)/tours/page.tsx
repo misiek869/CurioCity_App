ae30fae0af037db15f9d6a3f941ff8c7b66f65fe
Michal Gawik <gawlik.zator@o2.pl>
2025-07-16 05:47:19 +0200

    set up AllToursPage

--- src/app/(dashboard)/tours/page.tsx
+++ src/app/(dashboard)/tours/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import ToursPage from '@/components/ToursPage'
+import {
+	dehydrate,
+	HydrationBoundary,
+	QueryClient,
+} from '@tanstack/react-query'
 
-const ToursPage = () => {
-	return <div>tours</div>
+const AllToursPage = async () => {
+	const queryClient = new QueryClient()
+
+	await queryClient.prefetchQuery({
+		queryKey: ['tours'],
+		// queryFn: ()=>getAllTours()
+	})
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<ToursPage />
+		</HydrationBoundary>
+	)
 }
 
-export default ToursPage
+export default AllToursPage
