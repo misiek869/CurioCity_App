34764e8766edc28e7bdd7112aae070ea0082edca
Michal Gawik <gawlik.zator@o2.pl>
2025-07-28 13:12:05 +0200

    create search form

--- src/components/ToursPage.tsx
+++ src/components/ToursPage.tsx
@@ -3,16 +3,38 @@
 import { useQuery } from '@tanstack/react-query'
 import ToursList from './ToursList'
 import { getAllTours } from '@/utils/action'
+import { useState } from 'react'
 
 const ToursPage = () => {
+	const [searchTerm, setSearchTerm] = useState<string>('')
+
 	const { data, isPending } = useQuery({
-		queryKey: ['tours'],
-		queryFn: () => getAllTours(),
+		queryKey: ['tours', searchTerm],
+		queryFn: () => getAllTours(searchTerm),
 	})
 
 	console.log(data)
 	return (
 		<>
+			<form className='max-w-lg mb-12 '>
+				<div className='join w-full'>
+					<input
+						type='text'
+						placeholder='enter city name or country'
+						className='input input-ordered join-item w-full'
+						value={searchTerm}
+						onChange={e => setSearchTerm(e.target.value)}
+						required
+					/>
+					<button
+						type='button'
+						className='btn btn-primary join-item'
+						disabled={isPending}
+						onClick={() => setSearchTerm('')}>
+						{isPending ? 'wait...' : 'reset'}
+					</button>
+				</div>
+			</form>
 			{isPending ? (
 				<span className='loading'></span>
 			) : (
