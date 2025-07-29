b0d4a0aeb4c89f623eb8428966bfe55aad495f73
Michal Gawik <gawlik.zator@o2.pl>
2025-07-29 05:59:11 +0200

    add tour image to page

--- src/app/(dashboard)/tours/[id]/page.tsx
+++ src/app/(dashboard)/tours/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TourInfo from '@/components/TourInfo'
-import { getSingleTour } from '@/utils/action'
+import { generateTourImage, getSingleTour } from '@/utils/action'
+import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React from 'react'
@@ -10,11 +11,29 @@ const SingleTourPage = async ({ params }: { params: { id: string } }) => {
 	if (!tour) {
 		redirect('/tours')
 	}
+
+	const tourImg = await generateTourImage({
+		city: tour.city,
+		country: tour.country,
+	})
+
 	return (
 		<div>
 			<Link href={'/tours'} className='btn btn-secondary capitalize mb-12'>
 				back to tours
 			</Link>
+			{tourImg ? (
+				<div>
+					<Image
+						src={tourImg}
+						width={300}
+						height={300}
+						className='shadow-xl rounded-xl mb-12 h-96 w-96'
+						alt='panoramic view of the city'
+						priority
+					/>
+				</div>
+			) : null}
 			<TourInfo tour={tour} />
 		</div>
 	)
