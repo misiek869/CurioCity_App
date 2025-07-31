689e71d8fd61aa9f2ea0cd9fed8131daf8fa8adc
Michal Gawik <gawlik.zator@o2.pl>
2025-07-31 05:42:41 +0200

    ...

--- src/components/MemberProfile.tsx
+++ src/components/MemberProfile.tsx
@@ -1,9 +1,14 @@
+import { getUserTokensById } from '@/utils/action'
 import { UserButton } from '@clerk/nextjs'
 import { auth, currentUser } from '@clerk/nextjs/server'
 
 const MemberProfile = async () => {
 	const user = await currentUser()
-	// const { userId } = auth()
+	const session = await auth()
+	const userId = session.userId
+
+	console.log(userId)
+	console.log(await getUserTokensById(userId))
 
 	return (
 		<div className='px-4 flex items-center gap-2'>
